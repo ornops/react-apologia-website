a4983eddd914cf3625db2f527f37751b6edba593
ornops <joshuaornopia@gmail.com>
2024-04-26 00:25:25 +0800

    Started working on About Us Section

--- src/components/1-hero-section/hero-section.component.jsx
+++ src/components/1-hero-section/hero-section.component.jsx
@@ -4,10 +4,10 @@ const HeroSection = () =>{
 
     return(
         <div className="section-container hero-section">
-            <h1 className='hero-text'>Finding Joy in Christ</h1>
+            <h1 className='hero-text'>Apologia Gospel Church</h1>
             <div className='buttons-container'>
             <button className='hero-button schedule-button'>SCHEDULES</button>
-            <button className='hero-button streaming-button' >LIVE STREAMING</button>
+            <button className='hero-button streaming-button' >LATEST SERMON</button>
             </div>
             <div className='hero-bottom-paragraph'>
                 <p>Apologia Gospel Church Welcomes You Brethren!</p>
--- src/components/2-about-section/about-section.component.jsx
+++ src/components/2-about-section/about-section.component.jsx
@@ -4,7 +4,17 @@ const AboutSection = () =>{
 
     return(
         <div className="section-container about-section">
-            <h1>About Section</h1>
+            <div className='left-side'>
+                <h1 className='section-header-text about-header-text'>About Us</h1>
+                <img src="../../assets/about-image.jpg" alt="church" />
+            </div>
+            <div className='right-side'>
+                <p>
+                    We are a Christ-centered church that proclaims and defends the gospel.
+                    We are a community of believers that loves God and loves one another.
+                </p>
+
+            </div>
         </div>
     )
 }
