e3e58c6030a8a70e176b2f5ce48adaf5c28434c3
ornops <joshuaornopia@gmail.com>
2024-04-25 15:09:35 +0800

    Updated/Added elements inside the Hero Section

--- src/components/1-hero-section/hero-section.component.jsx
+++ src/components/1-hero-section/hero-section.component.jsx
@@ -5,8 +5,17 @@ const HeroSection = () =>{
     return(
         <div className="section-container hero-section">
             <h1 className='hero-text'>Finding Joy in Christ</h1>
+            <div className='buttons-container'>
+            <button className='hero-button schedule-button'>SCHEDULES</button>
+            <button className='hero-button streaming-button' >LIVE STREAMING</button>
+            </div>
+            <div className='hero-bottom-paragraph'>
+                <p>Apologia Gospel Church Welcomes You Brethren!</p>
+            </div>
+            
+            
+
         </div>
-        
     )
 }
 
