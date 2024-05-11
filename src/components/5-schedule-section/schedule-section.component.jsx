32fabcadf5c7fab84d31cda7846bcb884a2ead6c
ornops <joshuaornopia@gmail.com>
2024-05-11 14:40:09 +0800

    Created contents for Schedule section

--- src/components/5-schedule-section/schedule-section.component.jsx
+++ src/components/5-schedule-section/schedule-section.component.jsx
@@ -3,8 +3,35 @@ import './schedule-section.styles.css'
 const ScheduleSection = () =>{
 
     return(
-        <div className="section-container">
-            <h1>Schedule Section</h1>
+        <div className="section-container schedule-section">
+            <h1 className='section-header-text'>Daily Schedule</h1>
+            <table className='schedule-table'>
+                <tr>
+                    <th>Time</th>
+                    <th>Session</th>
+                    <th>Facilitator</th>
+                </tr>
+                <tr>
+                    <td>Sunday - 2:00 PM</td>
+                    <td>Sunday Fellowship</td>
+                    <td>Elders</td>
+                </tr>
+                <tr>
+                    <td>Sunday - 5:00 PM</td>
+                    <td>Small Group Fellowship</td>
+                    <td>Small Group Leaders</td>
+                </tr>
+                <tr>
+                    <td>Wednesday - 7:00 PM</td>
+                    <td>Prayer Meeting</td>
+                    <td>Assigned Facilitator</td>
+                </tr>
+                <tr>
+                    <td>Thursday - 7:00 PM</td>
+                    <td>Bereans Bible Study</td>
+                    <td>Bro. Kris</td>
+                </tr>
+            </table>
         </div>
     )
 }
