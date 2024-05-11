025a0928e91b83d041378d78b553877744d45957
ornops <joshuaornopia@gmail.com>
2024-05-11 14:55:07 +0800

    Added styling

--- src/components/5-schedule-section/schedule-section.component.jsx
+++ src/components/5-schedule-section/schedule-section.component.jsx
@@ -13,22 +13,22 @@ const ScheduleSection = () =>{
                 </tr>
                 <tr>
                     <td>Sunday - 2:00 PM</td>
-                    <td>Sunday Fellowship</td>
+                    <td className='session-bold'>Sunday Fellowship</td>
                     <td>Elders</td>
                 </tr>
                 <tr>
                     <td>Sunday - 5:00 PM</td>
-                    <td>Small Group Fellowship</td>
+                    <td className='session-bold'>Small Group Fellowship</td>
                     <td>Small Group Leaders</td>
                 </tr>
                 <tr>
                     <td>Wednesday - 7:00 PM</td>
-                    <td>Prayer Meeting</td>
+                    <td className='session-bold'>Prayer Meeting</td>
                     <td>Assigned Facilitator</td>
                 </tr>
                 <tr>
                     <td>Thursday - 7:00 PM</td>
-                    <td>Bereans Bible Study</td>
+                    <td className='session-bold'>Bereans Bible Study</td>
                     <td>Bro. Kris</td>
                 </tr>
             </table>
