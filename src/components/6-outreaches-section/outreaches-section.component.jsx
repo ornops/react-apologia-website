6e24233ba5b0af2706327269b2dd866cf5915e40
ornops <joshuaornopia@gmail.com>
2024-05-18 15:53:52 +0800

    Added images in the Outreaches section

--- src/components/6-outreaches-section/outreaches-section.component.jsx
+++ src/components/6-outreaches-section/outreaches-section.component.jsx
@@ -1,11 +1,24 @@
 import './outreaches-section.styles.css'
+import OutReachImage1 from '../../assets/outreach1.jpg';
+import OutReachImage2 from '../../assets/outreach2.jpg';
+import OutReachImage3 from '../../assets/outreach3.jpg';
+import OutReachImage4 from '../../assets/outreach4.jpg';
 
 const OutreachesSection = () =>{
 
     return(
         <div className="section-container outreaches-section">
             <div className="outreach-container">
-                <h1>Outreaches</h1>
+                <div className='header-container'>
+                    <h1 className='section-header-text'>Outreaches</h1>
+                </div>
+                
+                <div className="image-container">
+                <img src={OutReachImage1} className='outreach-image' alt="Inage 1" />
+                <img src={OutReachImage2} className='outreach-image' alt="Inage 1" />
+                <img src={OutReachImage3} className='outreach-image' alt="Inage 1" />
+                <img src={OutReachImage4} className='outreach-image' alt="Inage 1" />
+                </div>
             </div>
         </div>
     )
