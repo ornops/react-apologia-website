c0766b65a7aa8827bf06ba27ae4e3ba31f5b60a6
ornops <joshuaornopia@gmail.com>
2024-05-21 15:26:00 +0800

    Updated Contact Us Section

--- src/components/7-contact-section/contact-section.component.jsx
+++ src/components/7-contact-section/contact-section.component.jsx
@@ -4,7 +4,32 @@ const ContactSection = () =>{
 
     return(
         <div className="section-container contact-section">
-            <h1>Contact Us Section</h1>
+            <div className="image-banner-section">
+
+            </div>
+            <div className="contact-info-section">
+                <div className="contact-header">
+                    <h1>Contact Us</h1>
+                </div>
+                <div className="address-contact-container">
+                    <div className="address-container">
+                        <p>935 M Salinas Drive </p>
+                        <p>Cebu City</p> 
+                        <p>6000 Cebu</p>
+                    </div>
+                    <div className="contact-numbers-container">
+                        <p>Telephone Number : (123) 456 - 7890 </p>
+                        <p>Mobile Number : (123) 456 - 7890 </p> 
+                        <p>Email : apologiagospelchurchcebu@gmail.com</p>
+                    </div>
+                    
+                    
+                    
+                </div>
+                <div className="social-media-links">
+
+                </div>
+            </div>
         </div>
     )
 }
