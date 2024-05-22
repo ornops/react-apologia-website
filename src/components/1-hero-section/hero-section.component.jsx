8f997afb5954a6d6b6a7a03c94b09e1f5211f54f
ornops <joshuaornopia@gmail.com>
2024-05-22 15:32:29 +0800

    Updated Hero Section styling and image

--- src/components/1-hero-section/hero-section.component.jsx
+++ src/components/1-hero-section/hero-section.component.jsx
@@ -4,40 +4,12 @@ const HeroSection = () =>{
 
     return(
         <div className="section-container hero-section">
-            <div className="hero-text-container">
-                <p className='hero-text '>
-                    “All flesh is like grass 
-                </p>
-                <p className='hero-text '>
-                    and all its glory like the flower of grass.
-                </p>
-                <p className='hero-text'>
-                    The grass withers,
-                </p>
-                <p className='hero-text '>
-                    and the flower falls,
-                </p>
-                <p className='hero-text '>
-                    but the word of the Lord remains forever.”
-                </p>
-                <p className='hero-text'>
-                    And this word is the good news that was preached to you.
-                </p>
-            </div>
-           
-
             
             <div className='buttons-container'>
-                <a href="#schedule-section-anchor"><button className='hero-button schedule-button'>Schedule</button></a>
-                <a href='#sermon-section-anchor'><button className='hero-button streaming-button' >Latest Sermon</button></a>
+                <a href="#schedule-section-anchor"><button className='hero-button schedule-button'>SCHEDULE</button></a>
+                <a href='#sermon-section-anchor'><button className='hero-button sermon-button' >LATEST SERMON</button></a>
             
             </div>
-            <div className='hero-bottom-paragraph'>
-                <p>Apologia Gospel Church Welcomes You Brethren!</p>
-            </div>
-            
-            
-
         </div>
     )
 }
