bcc6afef6dbf4ea3201e3ce687c7015878b5753a
ornops <joshuaornopia@gmail.com>
2024-06-07 18:39:32 +0800

    Added paragraph in About section

--- src/components/2-about-section/about-section.component.jsx
+++ src/components/2-about-section/about-section.component.jsx
@@ -14,7 +14,8 @@ const AboutSection = () =>{
                     We are a Christ-centered church that proclaims and defends the gospel.<br/>
                     We are a community of believers that loves God and loves one another.<br/>
                     Apologia Gospel Church was organized, as a means to reflect on what a biblically sound,
-                    healthy, gospel-centered, Christ-exalting church should be<br/>
+                    healthy, gospel-centered, Christ-exalting church should be.<br/>
+                    Anything less is to fall short of what God requires of us as a church.
                 </p>
                 
 
